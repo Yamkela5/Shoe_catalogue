3cc0691a8e85a5b43bfd6920a25f6ba7155a38f1
Yamkela <owamjames1@gmail.com>
2017-04-22 12:10:42 +0200

    adding changes

--- Shoe_catalogue.js
+++ Shoe_catalogue.js
@@ -1,116 +1,90 @@
-//var temp =document.getElementById('.template')
-//var size= document.querySelectorAll('.size')
-//var color =document.querySelectorAll('.color')
-//var brandy =document.querySelectorAll('.brand')
+var Templ = document.querySelector('#template');
+var brandDp = document.querySelector('#template1');
+var sizeDp = document.querySelector('#template2');
+var colorDp = document.querySelector('#template3');
 
-    function Table() {
-      var shoes = [
-              {
-                  Color : 'blue',
-                  Price : 350,
-                  In_stock : 5,
-                  Size : 5,
-                  Brand : 'Nike'
-                },
+var mybtn = document.querySelector('#button');
+//console.log(mybtn);
 
 
 
+var shoes = [{
+    Color : 'black',
+    Price : 1350,
+    Instock : 15,
+    Size : 5,
+    Brand : 'Nike',
+    Image : 'nike black.jpg'
+  },
+  {
+    Color : 'white',
+    Price : 275,
+    Instock : 3,
+    Size : 7,
+    Brand : 'Adidas',
+    Image : 'nike black.jpg'
+  },
+  {
+    Color : 'Navy',
+    Price : 275,
+    Instock : 3,
+    Size : 3,
+    Brand : 'Sissy Boy',
+    Image : 'nike black.jpg'
+  },
+  {
+    Color : 'White',
+    Price : 1275,
+    Instock : 12,
+    Size : 7,
+    Brand : 'Batman'
+  },
+  {
+    Color : 'Pink',
+    Price : 905,
+    Instock : 12,
+    Size : 7,
+    Brand : 'Timberland',
+    Image : 'nike black.jpg'
+  },
+  {
+    Color : 'Grey',
+    Price : 2905,
+    Instock : 12,
+    Size : 6,
+    Brand : 'Asics',
+    Image : 'nike black.jpg'
+  },
+  {
+    Color : 'White',
+    Price : 805,
+    Instock : 12,
+    Size : 7,
+    Brand : 'Vans'
+}];
 
+// var Templ = document.querySelector('#template').innerHTML;
+//display dropdowns
+var comp = Handlebars.compile(colorDp.innerHTML);
+var result = comp({shoeData:shoes})
+console.log(result);
+var color =document.querySelector('.color')
+color.innerHTML=result;
+var comp = Handlebars.compile(sizeDp.innerHTML);
+var result = comp({shoeData:shoes})
+console.log(result);
+var size =document.querySelector('.size')
+size.innerHTML=result;
+var comp = Handlebars.compile(brandDp.innerHTML);
+var result = comp({shoeData:shoes})
+console.log(result);
+var brand =document.querySelector('.brand')
+brand.innerHTML=result;
+//dropdowns are now displaying
+var List = [];
 
-
-              {
-                  Color : 'black',
-                  Price : 275,
-                  In_stock : 3,
-                  Size : 7,
-                  Brand : 'Adidas'
-
-
-              },
-              {
-                  Color : 'Navy',
-                  Price : 275,
-                  In_stock : 3,
-                  Size : 3,
-                  Brand : 'Sissy Boy'
-
-
-              },
-              {
-                  Color : 'White',
-                  Price : 1275,
-                  In_stock : 12,
-                  Size : 7,
-                  Brand : 'Batman'
-
-              },
-              {
-                  Color : 'Pink',
-                  Price : 905,
-                  In_stock : 12,
-                  Size : 7,
-                  Brand : 'Timberland'
-
-              },
-
-              {
-                  Color : 'Grey',
-                  Price : 2905,
-                  In_stock : 12,
-                  Size : 6,
-                  Brand : 'Asics'
-
-
-              },
-              {
-                  Color : 'White',
-                  Price : 805,
-                  In_stock : 12,
-                  Size : 7,
-                  Brand : 'Vans'
-
-              },
-
-      ];
-
-
-        // EXTRACT VALUE FOR HTML HEADER.
-        // ('Book ID', 'Book Name', 'Category' and 'Price')
-        var col = [];
-        for (var i = 0; i < shoes.length; i++) {
-            for (var key in shoes[i]) {
-                if (col.indexOf(key) === -1) {
-                    col.push(key);
-                }
-            }
-        }
-
-        // CREATE DYNAMIC TABLE.
-        var table = document.createElement("table");
-
-        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
-
-        var tr = table.insertRow(-1);                   // TABLE ROW.
-
-        for (var i = 0; i < col.length; i++) {
-            var th = document.createElement("th");      // TABLE HEADER.
-            th.innerHTML = col[i];
-            tr.appendChild(th);
-        }
-
-        // ADD JSON DATA TO THE TABLE AS ROWS.
-        for (var i = 0; i < shoes.length; i++) {
-
-            tr = table.insertRow(-1);
-
-            for (var j = 0; j < col.length; j++) {
-                var tabCell = tr.insertCell(-1);
-                tabCell.innerHTML = shoes[i][col[j]];
-            }
-        }
-
-        // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-        var divContainer = document.getElementById("showData");
-        divContainer.innerHTML = "";
-        divContainer.appendChild(table);
-    }
+mybtn.addEventListener('click',function(){
+  //console.log('click');
+  var result = comp({shoe:shoes})
+  result;
+});
